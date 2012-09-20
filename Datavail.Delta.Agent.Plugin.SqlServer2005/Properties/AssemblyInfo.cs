74ac1343128bf8031204717c858aee9fed7a17b8
Build Agent <NT AUTHORITY\Build Agent>
2012-09-20 01:02:55 -0600

    ***NO_CI***

--- Datavail.Delta.Agent.Plugin.SqlServer2005/Properties/AssemblyInfo.cs
+++ Datavail.Delta.Agent.Plugin.SqlServer2005/Properties/AssemblyInfo.cs
@@ -34,5 +34,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("4.1.3085.0")]
-[assembly: AssemblyFileVersion("4.1.3085.0")]
+[assembly: AssemblyVersion("4.1.3086.0")]
+[assembly: AssemblyFileVersion("4.1.3086.0")]
