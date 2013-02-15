52875ba7048e8e805d736ee2a13e5e92da6e75a1
Matt Calhoun <matt.calhoun@datavail.com>
2013-02-15 13:32:48 -0500

    Deployment Changes

---
+++ Datavail.Delta.IncidentProcessor.Installer/Deploy.ps1
@@ -0,0 +1,13 @@
+﻿Get-Location | Write-Host
+Write-Host $OctopusPackageDirectoryPath
+
+$app = Get-WmiObject -Class Win32_Product | Where-Object { 
+    $_.Name -match "Delta Incident Processor" 
+}
+
+if($app)
+{
+$app.Uninstall()
+}
+
+& .\Datavail.Delta.IncidentProcessor.Installer.msi /qn
